40af88e765f80557807e4f9fbe6ccb32924eeae5
Rene Morgner <github@rmorgner.com>
2023-10-08 17:48:33 +0200

    add user router

---
+++ src/main/java/com/rmorgner/discorddatasupport/web/fn/UserRouter.java
@@ -0,0 +1,28 @@
+package com.rmorgner.discorddatasupport.web.fn;
+
+
+import com.rmorgner.discorddatasupport.constants.UserRestConstants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.*;
+import org.springframework.http.*;
+import org.springframework.web.reactive.function.server.*;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.*;
+
+@Configuration
+@RequiredArgsConstructor
+public class UserRouter {
+
+  private UserHandler userHandler;
+
+  @Bean
+  public RouterFunction<ServerResponse> userRoutes() {
+
+    return route()
+        .GET(UserRestConstants.PATH.getValue(), RequestPredicates.accept(MediaType.APPLICATION_JSON), userHandler::getAllUserIds)
+        .POST(UserRestConstants.PATH_WITH_ID.getValue(), RequestPredicates.accept(MediaType.APPLICATION_JSON), userHandler::saveUser)
+        .build();
+
+  }
+
+}
